85768c86e971e0716c731120312a2257248e2ce6
waldq <b14ef3@gmail.com>
2026-03-12 18:17:50 +0300

    Требования

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     username: str = Field(max_length=64)
     tag: str | None = Field(default=None, unique=True, min_length=5, max_length=32)
-    birthday: datetime | None = Field(default=None, )
+    birthday: datetime | None = Field(default=None)
     login: str = Field(unique=True, max_length=64)
     hashed_password: str
     bio: str | None = Field(default=None, max_length=140)
