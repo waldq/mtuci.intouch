b45a0848e5869ed09fc4efa2c1f20b9c5a66e837
liswxsh <polutenok@gmail.com>
2026-03-17 00:20:13 +0300

    Add React frontend

---
+++ front/src/components/pages/Register/register.jsx
@@ -0,0 +1,19 @@
+const Register = () => {
+    return(
+        <div>
+            <div>
+            <h2></h2>
+            <h5></h5>
+            <input placeholder="Номер телефона"></input>
+            <h5></h5>
+            <input placeholder="Пароль"></input>
+            <h5></h5>
+            <input placeholder="Подтвердите пароль"></input>
+            <button>Зарегестрироваться</button>
+            <button>Уже есть аккаунт?</button>
+            </div>
+        </div>
+    )
+};
+
+export default Register;
